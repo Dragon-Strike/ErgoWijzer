8ce2d125b7f2bce26b12700cb930e6c702a24e9f
Dragon-Strike <[540503@edu.rocmn.nl]>
2020-03-11 09:03:52 +0100

    html naar php gezet

--- index.php
+++ index.php
@@ -5,15 +5,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+    <link rel="stylesheet" href="style.css">
     <title>ErgoWijzer</title>
 </head>
 <body>
 
+
+<div id="header">
+    <h2 class="logo">ErgoWijzer</h2>
+    <input type="checkbox" id="chk">
+    <label for="chk" class="show-menu-btn">
+        <i class="fas fa-ellipsis-h"></i>
+    </label>
+
+    <ul class="menu">
+        <a href="#">Introductie</a>
+        <a href="#">Wat is RSI</a>
+        <a href="#">Contact</a>
+        <label for="chk" class="hide-menu-btn">
+            <i class="fas fa-times"></i>
+        </label>
+    </ul>
+</div>
+
+<div class="background-image1">
+    <iframe width="420" height="345" src="https://www.youtube.com/watch?v=nFH63-Ljk2I">
+    </iframe><h1>INTRODUCTIE</h1><h2>Beetje info</h2>
+</div>
+
+<div class="background-image2"><iframe width="420" height="345" src="https://www.youtube.com/watch?v=IC9X-GbFDc8">
+    </iframe><h1>WAT IS RSI ?</h1><h2>Beetje info</h2></div>
+
+<div class="background-image3"><iframe width="420" height="345" src="https://www.youtube.com/watch?v=HSVCTtEvzoY">
+    </iframe><h1>HOE VERKOM JE RSI ?</h1><h2>Beetje info</h2></div>
+
+
+
 </body>
 </html>
-<?php
-echo "hallo";
-echo "hoi, alles goed?";
-echo "hoi";
-echo "mmm";
-?>
